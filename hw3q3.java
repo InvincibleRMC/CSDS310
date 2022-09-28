c9302ae7da64d9320233b80b54df4fc49fb81db5
Michael Carlstrom <rmc@carlstrom.com>
2022-09-28 11:52:52 -0400

    h3q3 works

--- hw3q3.java
+++ hw3q3.java
@@ -3,167 +3,99 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//import java.util.Random;
-
-class hw3q3{
-public static void main(String[] args) {
-    // d is sorted
-    String dn =  "abcdefghijklmnop";
-    String[] d =dn.split("");
-   // String[] d = {"w","x","y","z"};
-    
-    // missing o
-    //string remove =
-    //String cn =  "legfihjdncamkpb";
-  //  String[] c =cn.split("");
-    //String[] c = {"y","z","w"};
-   // int n = c.length;
-    test(dn);
-   // System.out.println(missingString(d, c,0, n));
+class hw3q3 {
+    public static void main(String[] args) {
+        String dn = "abcdefghijklmnop";
+        test(dn);
     }
 
-    public static void test(String dn){
-        String[] d =dn.split("");
+    public static void test(String dn) {
+        String[] d = dn.split("");
         
-        for(int i=0;i<d.length;i++){
-            List<String> cList = new ArrayList(Arrays.asList(d));
-            String removed= (String) cList.remove(i);
-            for(int j=0;j<100;j++){
-            cList.add(cList.remove((int) Math.random()*cList.size()));
+        for (int i=0;i<d.length;i++) {
+            List<String> cList = new ArrayList<String>(Arrays.asList(d));
+            String removed = cList.remove(i);
+            for (int j=0;j<100;j++) {
+                cList.add(cList.remove((int) Math.random()*cList.size()));
             }
             String[] c =  cList.toArray(new String[cList.size()]);
             System.out.println("Missing:=" + removed);
-            String missing = missingString(d, c, 0, c.length);
-            if(!missing.equals(removed)){
+            String missing = missingString(d, c);
+            if (!missing.equals(removed)) {
                 throw new AssertionError(Arrays.asList(c) + " removed=" + removed + " missing=" + missing);
             }
             System.out.println("------------------------------------");
         }
-    
-
     }
 
-   public static String missingString(String[] d,String[] c,int low,int high){
-    System.out.println(Arrays.asList(c).subList(low, high) + " " +low + " " +high);
-   
-    int pivotD = (low+high)/2;
-    String pivotDVal = d[pivotD];
+    public static String missingString(String[] d, String[] c) {
+        System.out.println("START  d=" + Arrays.asList(d));
+        System.out.println("START  c=" + Arrays.asList(c));
+        return missingString(d, 0, d.length,
+                             c, 0, c.length);
+    }
+        
+    public static String missingString(String[] d, int dLow, int dHigh,
+                                       String[] c, int cLow, int cHigh) {
+        // base case - only one element left in d, must be missing string
+        if ((dHigh - dLow) == 1) {
+            String missing = d[dLow];
+            System.out.println("MISSING=" + missing);
+            return missing;
+        }
 
-    int bestPivotValLocation=-10000;
-
+        System.out.println();
+        System.out.println("BEFORE d=" + Arrays.asList(d).subList(dLow, dHigh) + " dLow=" +dLow + " dHigh=" +dHigh);
+        System.out.println("BEFORE c=" + Arrays.asList(c).subList(cLow, cHigh) + "    cLow=" +cLow + " cHigh=" +cHigh);
+        int pivotD = (dLow+dHigh)/2;
+        String pivotDVal = d[pivotD];
+        System.out.println("pivotD=" + pivotD + " pivotDval=" + pivotDVal);
     
-    System.out.println("PivotValInC " + bestPivotValLocation + " PivotVal " + pivotDVal );
+        int pivotC = partition(c, cLow, cHigh, pivotDVal);
+        System.out.println("pivotC=" + pivotC);
+        System.out.println("AFTER  c=" + Arrays.asList(c).subList(cLow, cHigh) + " cLow=" +cLow + " cHigh=" +cHigh);
 
-    
-   // boolean left=();
-    int pivotC = partition(d, c, low, high,pivotDVal);
-   System.out.println(Arrays.asList(c).subList(low, high) + " " +low + " " +high);
-   
+        int leftAmountD  = pivotD-dLow;
+        int leftAmountC  = pivotC-cLow;
+        System.out.println("leftAmountD=" + leftAmountD + " leftAmountC=" + leftAmountC);
 
-    int leftAmountC = pivotC-low;
-    int rightAmountC = high-pivotC;
-
-    int leftAmountD = pivotD-low;
-    int rightAmountD =high-pivotD;
-
-    //System.out.println(Arrays.asList(c).subList(low, high) + " " +low + " " +high);
-   
-
-    for(int i =low;i<high;i++){
-        if(c[i].equals(d[pivotC])){
-            bestPivotValLocation=i;
-            break;
+        // look right
+        if (leftAmountD == leftAmountC) {
+            System.out.println("looking right in d");
+            return missingString(d, pivotD, dHigh,
+                                 c, pivotC, cHigh);
+        }
+        // look left
+        else{
+        System.out.println("looking left in d");
+        return missingString(d, dLow, pivotD,
+                             c, cLow, pivotC);
         }
     }
-    if(bestPivotValLocation==-10000){
-        System.out.println("val is " + d[pivotC]);
-        return d[pivotC];
-    }
-    
-   System.out.println("pivotC= " + pivotC); //+ " d[pivotC]= " + d[pivotC] );
-    System.out.println(" " + leftAmountC + " " + leftAmountD + " " + rightAmountC + " " + rightAmountD + " "  );
-       
-    //look right
-    if(rightAmountC < leftAmountC){
-        //pivotC= partition(d, c, bestPivotValLocation, high);
-        //System.out.println(pivotC);
-        System.out.println("looking right");
-        return missingString(d, c, pivotC+1,high);
-    }
-    //look left
-    else if (rightAmountC > leftAmountC){
-       // pivotC= partition(d, c, low, bestPivotValLocation);
-        System.out.println("looking left");
-        return missingString(d, c, low,pivotC); 
-    }
-    
-    
-    
-    //look right
-        if(rightAmountC == rightAmountD){
-            //pivotC= partition(d, c, bestPivotValLocation, high);
-            //System.out.println(pivotC);
-            System.out.println("looking right");
-            return missingString(d, c, pivotC+1,high);
-        }
-        //look left
-        else if (leftAmountC == leftAmountD){
-           // pivotC= partition(d, c, low, bestPivotValLocation);
-            System.out.println("looking left");
-            return missingString(d, c, low,pivotC); 
-        }
-       return "yikes";
-   }
-    public static int partition(String[] d, String[] arr, int low, int high, String pivotDVal)
+
+    // returns lower bound of right partition
+    public static int partition(String[] arr, int low, int high, String value)
     {
-       // int pivotD=(int) Math.ceil((low+high)/2);
-       // String pivotDVal = d[pivotD];
-        int pivotC = low;
+        // System.out.println("PARTITION IN value=" + value + " arr=" + Arrays.asList(arr).subList(low, high));
+        int pivot = low;
         for (int i = low; i < high; i++) {
             // inserting elements of less value
             // to the left of the pivot location
-            if (arr[i].compareTo(pivotDVal)<=0) {
-                swap(arr, i, pivotC);
-                pivotC++;
+            if (arr[i].compareTo(value) < 0) {
+                swap(arr, i, pivot);
+                pivot++;
             }
+            // System.out.println("PARTITION left=" + Arrays.asList(arr).subList(low, pivot)
+            //                    + " value=" + value
+            //                    + " right=" + Arrays.asList(arr).subList(pivot, high));
         }
-  
-        // swapping pivot to the final pivot location
-        swap(arr, high-1, pivotC);
-        //System.out.println(pivotC);
-        return pivotC;
+        // System.out.println("PARTITION OUT arr=" + Arrays.asList(arr).subList(low, high) + " pivot=" + pivot);
+        return pivot;
     }
 
-    public static void swap(String[] arr, int i, int j){
-       //System.out.println("i:" +i + "j:" + j);
+    public static void swap(String[] arr, int i, int j) {
         String temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-    
-    public static int goodPivot(String[] d, String[] c, int l,int h){
-        String lookingfor = d[(l+h)/2];
-        System.out.println(lookingfor);
-        for(int i=l;i<l;i++){
-            if(lookingfor.compareTo(c[i])==0)
-            {
-                System.out.println(i);
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public static boolean exists(String[] d , String c,int low,int high){
-
-        for(int i =0;i<d.length;i++){
-            if(c.equals(d[i])){
-                return true;
-            }
-        }
-
-        //System.out.println(Arrays.binarySearch(Arrays.sort(d), c) >= 0);
-        return false;
-    }
-
 }
