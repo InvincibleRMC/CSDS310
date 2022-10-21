09458b60e2cf63f3042a912ae35f5d746309acfe
Michael Carlstrom <rmc@carlstrom.com>
2022-10-20 22:18:03 -0400

    just global bro

--- hw4.m
+++ hw4.m
@@ -1,56 +1,11 @@
+global counter
+counter =1;
 values = [3 4 2 6 0.5];
 operations = ["+" "x" "+" "x"];
 
-n = length(values);
-
-m=zeros(n);
-s=zeros(n-1);
-for i = 1:n
-    m(i,i)=values(i);
-end
-
-for l =2:n
-    for i =1:(n-l+1)
-        j=i+l-1;
-        m(i,j) = 0;
-        for k =i:(j-1)
-            q = helper(m(i,k),m(k+1,j),operations(k));
-            if q > m(i,j)
-                m(i,j) = q;
-                s(i,j) = k;
-            end
-        end
-    end
-end
-disp(m);
-disp(s);
-paren(values,operations,s,1,n);
-
-function q = helper(left,right,o)
-    if(strcmp(o,"+"))
-        q = left+right;
-    else
-        q=left*right;
-    end
-end
-
-function paren(values,operations,s,i,j)
-    if i==length(values)
-        fprintf("%d",values(i));
-        return;
-    end
-    if i==j
-        fprintf("%d %s ",values(i),operations(i));
-    else
-     fprintf("(");
-     paren(values,operations,s,i,s(i,j))values = [3 4 2 6 0.5];
-operations = ["+" "x" "+" "x"];
-
 [m,s] = expres(values,operations);
-
-disp(m);
-disp(s);
 paren(values,operations,s,1,n);
+counter =1;
 
 function [m,s] = expres(values,operations)
     n = length(values);
@@ -84,20 +39,21 @@ function q = helper(left,right,o)
     end
 end
 
+
 function paren(values,operations,s,i,j)
+global counter;
+
     if i==length(values)
         fprintf("%d",values(i));
         return;
     end
     if i==j
-        fprintf("%d %s ",values(i),operations(i));
+        fprintf("%d ",values(i));
     else
      fprintf("(");
      paren(values,operations,s,i,s(i,j))
-     paren(values,operations,s,s(i,j)+1,j)
-     fprintf(")");
-    end
-end
+     fprintf("%s ",operations(counter));
+     counter = counter +1;
      paren(values,operations,s,s(i,j)+1,j)
      fprintf(")");
     end
